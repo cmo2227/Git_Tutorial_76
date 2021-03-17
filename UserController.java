b440669a96b99698d84180040b63c0b452ec92f6
cmo2227 <cmo2227@naver.com>
2021-03-17 10:12:40 +0900

    Add UserController [Login]

---
+++ UserController.java
@@ -0,0 +1,21 @@
+@Controller
+public class UserController{
+	//로그인 페이지 이동
+	@RequestMapping("/user/login.do")
+	public String login(){
+		logger.info("경로:login");
+		return "user/login";
+	}
+
+	@RequestMapping("/user/insert.do")
+	public String insert(){
+		logger.info("경로:insert");
+		return "user/insert";
+	}
+
+	@RequestMapping("/user/update.do")
+	public String update(){
+		logger.info("경로:update");
+		return "user/update";
+	}
+}
